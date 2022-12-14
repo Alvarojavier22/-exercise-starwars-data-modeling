bc657822d9f17a0630a5bed6771fadd89c4a468f
Alvaro Javier <alvarojavierchagas@hotmail.com>
2022-12-14 02:15:37 +0000

    Pronto

--- src/models.py
+++ src/models.py
@@ -8,23 +8,70 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    fav_planets = relationship('Fav_planets', backref='user', lazy=True)
+    fav_characters= relationship('Fav_characters', backref='user', lazy=True)
+    fav_species = relationship('Fav_species', backref='user', lazy=True)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Planets(Base):
+    __tablename__ = 'planets'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    planet_name = Column(String)
+    planet_diameter = Column(String)
+    planet_gravity = Column(String)
+    planet_population = Column(String)
+    planet_climate = Column(String)
+    planet_terrain = Column(String)
+    planet_created = Column(String)
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    character_name = Column(String)
+    character_height = Column(String)
+    character_mass = Column(String)
+    character_gender = Column(String)
+    character_created = Column(String)
+    character_homeworld = Column(String)
+    character_url = Column(String)
+
+class Species(Base):
+    __tablename__ = 'species'
+    id = Column(Integer, primary_key=True)
+    species_name = Column(String)
+    species_clasification = Column(String)
+    species_designation = Column(String)
+    species_homeworld = Column(String)
+    species_language = Column(String)
+    species_people = Column(String)
+    species_crated = Column(String)
+
+class Fav_planets(Base):
+    __tablename__ = 'fav_planets'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    planets_id = Column(Integer, ForeignKey('planets.id'))
+
+class Fav_characters(Base):
+    __tablename__ = 'fav_characters'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    characters_id = Column(Integer, ForeignKey('characters.id'))
+
+class Fav_species(Base):
+    __tablename__ = 'fav_species'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    species_id = Column(Integer, ForeignKey('species.id'))
 
     def to_dict(self):
         return {}
